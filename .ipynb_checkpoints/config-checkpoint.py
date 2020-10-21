3837af5d0ef15c3c8c36e4acae2f8de43a1d1c72
chilli <70721729+hema2575@users.noreply.github.com>
2020-10-20 22:58:01 -0500

    Completed code and all scatter plots and heatmap images

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 15:08:03 2020
+
+@author: muthukumar
+"""
+
+# OpenWeatherMap API Key
+weather_key = "397b95f1b822905242cabd4b62006f61"
+weather_api_key = "a523a536ac08b08a2413d1ec108164e9"
+# Google API Key
+g_key = "AIzaSyA-MmN8Y965uvvQICNfhQEUUsKDRlp22uc"

60bd2c6ff3fbef161fb8ab20523ed17116ae6354
chilli <70721729+hema2575@users.noreply.github.com>
2020-10-22 22:27:06 -0500

    put jupyternb files into folders and added a gitignore to ignore config files

---
+++ WeatherPy/config1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 15:56:06 2020
+
+@author: muthukumar
+"""
+
+w_key="397b95f1b822905242cabd4b62006f61"
+# Google API Key
+g_key = "AIzaSyCRlsom3w_UNw2Qp1hb7UzASUz7LiUop8U"
